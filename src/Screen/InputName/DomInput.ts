070eb6a31510187ed94bad32e68547096aad66bd
2dva <x7bb@mail.ru>
2026-03-13 11:37:04 +0300

    dom input improvement

--- src/Screen/InputName/DomInput.ts
+++ src/Screen/InputName/DomInput.ts
@@ -1,11 +1,13 @@
-import fontStyles from '../../fontStyles.json'
+import { uid } from 'pixi.js'
+import './style.css'
 
 export class DomInput extends HTMLInputElement {
   constructor(filter?: RegExp) {
     super()
+    this.id = 'inputName' + uid()
     this.type = 'text'
     this.maxLength = 6
-    Object.assign(this.style, fontStyles.fontStyleInputName)
+    this.className = 'cl-screenname-input'
     if (filter) {
       this.addEventListener('input', () => {
         this.value = this.value.replace(filter, '')
